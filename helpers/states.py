b6b5e5c57394aa0becd5baaf2a8bf28337ac5827
yfree1232-afk <yfree1232@gmail.com>
2026-09-17 21:33:45 +0530

    Upgrade to Aiogram 3: helpers/states.py

---
+++ helpers/states.py
@@ -0,0 +1,21 @@
+﻿from aiogram.fsm.state import State, StatesGroup
+
+class GenerateStates(StatesGroup):
+    waiting_custom_api_id = State()
+    waiting_custom_api_hash = State()
+    waiting_phone = State()
+    waiting_otp = State()
+    waiting_2fa = State()
+
+class DeviceStates(StatesGroup):
+    waiting_session = State()
+
+class VaultStates(StatesGroup):
+    waiting_session = State()
+
+class ToolStates(StatesGroup):
+    waiting_leave_session = State()
+    waiting_health_session = State()
+
+class AdminStates(StatesGroup):
+    waiting_broadcast_msg = State()
