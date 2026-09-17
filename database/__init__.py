1cbbfb1f0830b73c579e6432992e9c266c479fa7
yfree1232-afk <yfree1232@gmail.com>
2026-09-17 21:57:58 +0530

    Upgrade with MongoDB Atlas & Aiogram 3: database/__init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,19 @@
-﻿# Database package init
-from .db import db
+# Database package init
+from .db import (
+    db, init_db, add_user, register_user, get_user, get_users_count, get_all_users,
+    save_account, save_or_update_account, get_user_accounts, get_account, get_account_by_db_id,
+    toggle_account_active, toggle_all_accounts, update_account_status, delete_account,
+    count_user_accounts, update_vc_message, toggle_vc_auto_send, update_vc_delay,
+    has_sent_vc_dm, log_vc_dm, count_vc_dms_sent, log_activity, get_recent_activity,
+    sync_global_sessions_for_user, DEFAULT_VC_MESSAGE
+)
 
-__all__ = ["db"]
+__all__ = [
+    "db", "init_db", "add_user", "register_user", "get_user", "get_users_count", "get_all_users",
+    "save_account", "save_or_update_account", "get_user_accounts", "get_account", "get_account_by_db_id",
+    "toggle_account_active", "toggle_all_accounts", "update_account_status", "delete_account",
+    "count_user_accounts", "update_vc_message", "toggle_vc_auto_send", "update_vc_delay",
+    "has_sent_vc_dm", "log_vc_dm", "count_vc_dms_sent", "log_activity", "get_recent_activity",
+    "sync_global_sessions_for_user", "DEFAULT_VC_MESSAGE"
+]
+
